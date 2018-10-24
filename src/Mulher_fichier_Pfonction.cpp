521a9a922ef63dcd1d5c334e4cff841bd7f3c086
Mildred34 <alexis-h-34@hotmail.fr>
2018-10-24 17:12:17 +0200

    Ajout des fichiers bitmaps

--- src/Mulher_fichier_Pfonction.cpp
+++ src/Mulher_fichier_Pfonction.cpp
@@ -8,8 +8,32 @@
 
 #include <iostream>
 using namespace std;
+#include <stdio.h>
+#define max 100
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	FILE* fichier = NULL;
+	fichier = fopen("untexte.txt","w+");
+
+	int age = 0;
+	char chaine[max] = "NULL";
+
+	if(fichier != NULL)
+	{
+		cout << "quel est votre âge" << endl;
+		cin >> age;
+
+		fprintf(fichier,"L'age du dev est de %d ans",age);
+		rewind(fichier);
+		fgets(chaine,10,fichier);
+		cout << "la chaine est: " << chaine << endl;
+
+		fclose(fichier);
+	}
+	else{
+		cout << "Petit problème de lecture" << endl;
+	}
+
+
 	return 0;
 }
